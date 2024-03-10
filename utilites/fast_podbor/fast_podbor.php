4b92a77ccca6ba423e30ec112a4b5927a3e900dd
DonaterSan <134006061+DonaterSan@users.noreply.github.com>
2024-03-10 14:57:03 +0300

    test_home

--- utilites/fast_podbor/fast_podbor.php
+++ utilites/fast_podbor/fast_podbor.php
@@ -12,115 +12,130 @@
     <div class="form-popup" id="myForm">
         <div id="popup">
             <div class="close_container">
-                <img src="" alt="" class="close" onclick="closeForm()">
+                <img src="img/icon/img.png" alt="" class="close" id="close" onclick="closeForm()">
             </div>
-            <h1></h1>
-            <p></p>
-            <form class="section1" id="openForm__1">
-                <div class="form_top">
-                    <div class="circle">
-                        <label for="form_1-check-man">папе/парню</label>
-                        <input type="radio" id="form_1-check-man" name="check">
-                    </div>
-                    <div>
-                        <label for="form_1-check-girl1">девушке/жене</label>
-                        <input type="radio" id="form_1-check-girl1" name="check">
-                    </div>
-                    <div>
-                        <label for="form_1-check-girl2">маме/бабушке</label>
-                        <input type="radio" id="form_1-check-girl2" name="check">
-                    </div>
-                </div>
-
-                <br>
-
-                <div class="form_bot">
-                    <label for="form_1-check-girl3">подруге</label>
-                    <input type="checkbox" id="form_1-check-girl3" name="check">
-
-                    <label for="form_1-check-chief">начальнику</label>
-                    <input type="checkbox" id="form_1-check-chief" name="check">
-
-                    <label for="form_1-check-classic">классика</label>
-                    <input type="checkbox" id="form_1-check-classic" name="check">
-                </div>
-
-                <button class="btn" type="submit" onclick="openForm__2()">Далее</button>
-            </form>
-            <form action="" class="section2" id="Form__2">
-                <div>
+            <form action="" id="myForm">
+                <div class="section1" id="openForm__1">
+                    <h1 class="title_podbor">Быстрый подбор букета</h1>
+                    <p>Для кого вы подбираете букет?</p>
                     <div class="form_top">
-                        <div>
-                            <label for="form_1-check-man">до 2000 ₽ </label>
-                            <input type="checkbox" id="form_1-check-man" name="check">
+                        <div class="circle">
+                            <input type="radio" id="form_1-check-man" name="check" class="circle-radio">
+                            <label for="form_1-check-man">папе/парню</label>
                         </div>
-                        <div>
-                            <label for="form_1-check-girl1">от 2000 ₽ до 3500 ₽</label>
-                            <input type="checkbox" id="form_1-check-girl1" name="check">
+                        <div class="circle">
+                            <input type="radio" id="form_1-check-girl1" name="check" class="circle-radio">
+                            <label for="form_1-check-girl1">девушке/жене</label>
                         </div>
-                        <div>
-                            <label for="form_1-check-girl2">от 3500 ₽ до 5000 ₽</label>
-                            <input type="checkbox" id="form_1-check-girl2" name="check">
+                        <div class="circle">
+                            <input type="radio" id="form_1-check-girl2" name="check" class="circle-radio">
+                            <label for="form_1-check-girl2">маме/бабушке</label>
                         </div>
                     </div>
 
                     <br>
 
                     <div class="form_bot">
-                        <label for="form_1-check-girl3">свыше 5000 ₽</label>
-                        <input type="checkbox" id="form_1-check-girl3" name="check">
+                        <div class="circle">
+                            <input type="radio" id="form_1-check-girl3" name="check" class="circle-radio">
+                            <label for="form_1-check-girl3">подруге</label>
+                        </div>
+                        <div class="circle">
+                            <input type="radio" id="form_1-check-chief" name="check" class="circle-radio">
+                            <label for="form_1-check-chief">начальнику</label>
+                        </div>
+                        <div class="circle">
+                            <input type="radio" id="form_1-check-classic" name="check" class="circle-radio">
+                            <label for="form_1-check-classic">классика</label>
+                        </div>
                     </div>
 
-                    <button class="btn_back" onclick="openForm__1()">Назад</button>
-                    <button class="btn_next" type="submit" onclick="openForm__3()">Далее</button>
+                    <button class="btn_next" type="button" onclick="openForm__2()">Далее</button>
                 </div>
-            </form>
-            <form action="" class="section3" id="Form__3">
-                <div>
-                    <div class="form_top">
-                        <div>
-                            <label for="form_1-check-man">Роза</label>
-                            <input type="checkbox" id="form_1-check-man" name="check">
-                        </div>
-                        <div>
-                            <label for="form_1-check-girl1">Хризантема</label>
-                            <input type="checkbox" id="form_1-check-girl1" name="check">
-                        </div>
-                        <div>
-                            <label for="form_1-check-girl2">Лилия</label>
-                            <input type="checkbox" id="form_1-check-girl2" name="check">
+                <div class="section2" id="openForm__2">
+                    <h1 class="title_podbor">Быстрый подбор букета</h1>
+                    <p>Ваш бюджет</p>
+                    <div>
+                        <div class="form_top">
+                            <div>
+                                <input type="radio" id="form_1-check-man" name="check" class="circle-radio">
+                                <label for="form_1-check-man">до 2000 ₽ </label>
+                            </div>
+                            <div>
+                                <input type="radio" id="form_1-check-girl1" name="check" class="circle-radio">
+                                <label for="form_1-check-girl1">от 2000 ₽ до 3500 ₽</label>
+                            </div>
+                            <div>
+                                <input type="radio" id="form_1-check-girl2" name="check" class="circle-radio">
+                                <label for="form_1-check-girl2">от 3500 ₽ до 5000 ₽</label>
+                            </div>
                         </div>
 
-                        <label for="form_1-check-chief">Орхидея</label>
-                        <input type="checkbox" id="form_1-check-girl3" name="check">
+                        <br>
 
+                        <div class="form_bot">
+                            <div class="circle">
+                                <input type="radio" id="form_1-check-girl3" name="check" class="circle-radio">
+                                <label for="form_1-check-girl3">свыше 5000 ₽</label>
+                            </div>
+                        </div>
+
+                        <button class="btn_back" type="button" onclick="openForm()">Назад</button>
+                        <button class="btn_next" type="button" onclick="openForm__3()">Далее</button>
                     </div>
-
-                    <br>
-
-                    <div class="form_bot">
-
-                        <label for="form_1-check-chief">Гербера</label>
-                        <input type="checkbox" id="form_1-check-chief" name="check">
-
-                        <label for="form_1-check-classic">Не знаю</label>
-                        <input type="checkbox" id="form_1-check-classic" name="check">
-
-                    </div>
-
-                    <button class="btn_back" onclick="openForm__2()">Назад</button>
-                    <button class="btn_next" type="submit" onclick="openForm__4()">Далее</button>
                 </div>
-            </form>
-            <form action="" id="openForm__4" class="section3">
-                <div>
-                    <div class="form_top">
-                        <div>
-                            <p>Мы сделали подборку букетов по вашим запросам, вам остается только выбрать понравившейся!</p>
+                <div class="section3" id="openForm__3">
+                    <h1 class="title_podbor">Быстрый подбор букета</h1>
+                    <p>Есть предпочтение по цветам?</p>
+                    <div class="">
+                        <div class="form_top">
+                            <div class="circle">
+                                <input type="radio" id="form_1-check-man" name="check" class="circle-radio">
+                                <label for="form_1-check-man">Роза</label>
+                            </div>
+                            <div class="circle">
+                                <input type="radio" id="form_1-check-girl1" name="check" class="circle-radio">
+                                <label for="form_1-check-girl1">Хризантема</label>
+                            </div>
+                            <div class="circle">
+                                <input type="radio" id="form_1-check-girl2" name="check" class="circle-radio">
+                                <label for="form_1-check-girl2">Лилия</label>
+                            </div>
+                            <div class="circle">
+                                <input type="radio" id="form_1-check-girl3" name="check" class="circle-radio">
+                                <label for="form_1-check-chief">Орхидея</label>
+                            </div>
                         </div>
-                    </div>
 
-                    <button class="btn_back" onclick="">Посмотреть</button>
+                        <br>
+
+                        <div class="form_bot">
+                            <div class="circle">
+                                <input type="radio" id="form_1-check-chief" name="check" class="circle-radio">
+                                <label for="form_1-check-chief">Гербера</label>
+                            </div>
+                            <div class="circle">
+                                <input type="radio" id="form_1-check-classic" name="check" class="circle-radio">
+                                <label for="form_1-check-classic">Не знаю</label>
+                            </div>
+                        </div>
+
+                        <button class="btn_back" type="button" onclick="openForm__2()">Назад</button>
+                        <button class="btn_next" type="button" onclick="openForm__4()">Далее</button>
+                    </div>
+                </div>
+                <div id="openForm__4" class="section3">
+                    <h1 class="title_podbor">Быстрый подбор букета</h1>
+                    <p>Готово!</p>
+                    <div>
+                        <div class="form_bot">
+                            <div>
+                                <p class="text__sec4">Мы сделали подборку букетов по вашим запросам, вам остается только выбрать понравившейся!</p>
+                            </div>
+                        </div>
+
+                        <button class="btn_next" id="btn_next" type="submit" onclick="">Посмотреть</button>
+                    </div>
                 </div>
             </form>
         </div>
@@ -136,24 +151,33 @@
         }
 
         function openForm() {
+            document.getElementById("close").style.display = "block";
             document.getElementById("myForm").style.display = "block";
             document.getElementById("openForm__1").style.display = "block";
             document.getElementById("back").style.display = "block";
+            document.getElementById("openForm__2").style.display = "none";
+            document.getElementById("openForm__3").style.display = "none";
+            document.getElementById("openForm__4").style.display = "none";
         }
         function openForm__2() {
-            document.getElementById("myForm__2").style.display = "block";
-            document.getElementById("myForm__1").style.display = "none";
+            document.getElementById("openForm__2").style.display = "block";
+            document.getElementById("openForm__1").style.display = "none";
+            document.getElementById("openForm__3").style.display = "none";
             document.getElementById("back").style.display = "block";
+            document.getElementById("openForm").style.display = "none";
         }
         function openForm__3(){
-            document.getElementById("myForm__3").style.display = "block";
-            document.getElementById("myForm__2").style.display = "none";
+            document.getElementById("openForm__3").style.display = "block";
+            document.getElementById("openForm__2").style.display = "none";
             document.getElementById("back").style.display = "block";
         }
         function openForm__4(){
-            document.getElementById("myForm__4").style.display = "block";
-            document.getElementById("myForm__3").style.display = "none";
+            document.getElementById("openForm__4").style.display = "block";
+            document.getElementById("openForm__3").style.display = "none";
+            document.getElementById("openForm__2").style.display = "none";
+            document.getElementById("openForm__1").style.display = "none";
             document.getElementById("back").style.display = "block";
+            document.getElementById("btn_next").style.marginTop = "0";
         }
     </script>
 </div>
